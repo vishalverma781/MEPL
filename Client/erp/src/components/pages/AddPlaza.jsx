f83f9fb6c4b6424742cc87aece64a9d75b032291
vishalverma <vishalverma@gmail.com>
2026-01-27 16:45:23 +0530

    first commit

--- Client/erp/src/components/pages/AddPlaza.jsx
+++ Client/erp/src/components/pages/AddPlaza.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FaBuilding, FaTasks } from "react-icons/fa";
+import { FaChevronDown } from "react-icons/fa";
 
 const AddPlaza = () => {
   const [formData, setFormData] = useState({
